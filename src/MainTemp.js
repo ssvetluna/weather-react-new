b08f49d95aaa41e6b69d1c6d094d5c767cd000f9
ssvetluna <s.stepanenko.a@gmail.com>
2022-09-29 21:51:20 +0300

    Delete C/F convertation

--- src/MainTemp.js
+++ src/MainTemp.js
@@ -1,52 +1,12 @@
 import React from "react";
-import { useState } from "react";
 
 import "./MainTemp.css";
 
 export default function MainTemp(props) {
-  let [count, setCount] = useState(1);
-  function handleClickCelsius(event) {
-    event.preventDefault();
-    setCount(1);
-  }
-  function handleClickFarengeit(event) {
-    event.preventDefault();
-    setCount(2);
-  }
-  if (count === 1) {
-    return (
-      <div className="Units">
-        <span className="MainTemp">{Math.round(props.temp)}</span>
-        <span>
-          <a href="/" className="Celsius" onClick={handleClickCelsius}>
-            °C
-          </a>
-        </span>
-        <span className="slash">|</span>
-        <span>
-          <a href="/" className="Farengeit" onClick={handleClickFarengeit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="Units">
-        <span className="MainTemp">
-          {Math.round((props.temp * 9) / 5 + 32)}
-        </span>
-        <span>
-          <a href="/" className="Celsius" onClick={handleClickCelsius}>
-            °C |
-          </a>
-        </span>
-        <span>
-          <a href="/" className="Farengeit" onClick={handleClickFarengeit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="Units">
+      <span className="MainTemp">{Math.round(props.temp)}</span>
+      <span className="Celsius">°C</span>
+    </div>
+  );
 }
